669f1c8b3d48d83b87200c0d1d3ee0b8db6082ee
Gaurav4code <85581658+Gaurav4code@users.noreply.github.com>
2021-12-03 20:46:19 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+import List from "./List";
+import data from "./data";
+import { useState } from "react";
+
+export default function App() {
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear All</button>
+      </section>
+    </main>
+  );
+}
